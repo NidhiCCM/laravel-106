00930ea9097c8880e6eeb5ae5c122d1a4f36e8d0
Nidhi Sharma <nidhi.sharma@canvascraft.media>
2023-07-18 15:39:30 +0530

    Fourth Commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('roles.index')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')">
                         {{ __('Roles') }}
                     </x-nav-link>
                 </div>
