dec88104194c41925877e85ae6ce8a2eacaff005
Nidhi Sharma <nidhi.sharma@canvascraft.media>
2023-07-20 10:28:51 +0530

    Validate the data

--- app/Http/Middleware/EnsureRoleOwner.php
+++ app/Http/Middleware/EnsureRoleOwner.php
@@ -4,9 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
-use App\Models\Role;
 
 class EnsureRoleOwner
 {
@@ -17,7 +15,7 @@ class EnsureRoleOwner
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ( $request->role->user_id !== Auth::user()->id ) {
+        if ( $request->role->user_id !== $request->user()->id ) {
             return redirect(route('roles.index'))->with('error', 'Unauthorized Page');
           }
 
